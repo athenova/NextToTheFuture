dd315137f3acdc06b2485f6d7ada38177a8b1167
athenova <aleksey.sergeyev@yandex.com>
2025-03-15 19:22:08 +0300

    Перевод на simple-blogger, постер Инстаграм

---
+++ project.py
@@ -0,0 +1,50 @@
+from simple_blogger import CommonBlogger
+from simple_blogger.generators.OpenAIGenerator import OpenAITextGenerator
+from datetime import datetime
+from datetime import timedelta
+from simple_blogger.senders.TelegramSender import TelegramSender
+from simple_blogger.senders.InstagramSender import InstagramSender
+
+class Project(CommonBlogger):
+    def _example_task_creator(self):
+        return [
+            {
+                "problem": "problem",
+                "technic": "technic",
+                "description": "description",
+                "group": "group"
+            }
+        ]
+
+    def _get_category_folder(self, task):
+        return f"{task['group']}/{task['technic']}"
+                    
+    def _get_topic_folder(self, task):
+        return f"{task['problem']}"
+
+    def _system_prompt(self, task):
+        return "Ты - коуч будущего с передовыми техниками, нестандартными инсайтами и проверенными стратегиями, которые помогут твоим клиентам создать свою новую реальность"
+
+    def _task_converter(self, idea):
+        return { 
+                    "group": idea['group'],
+                    "technic": idea['technic'],
+                    "problem": idea['problem'],
+                    "description_prompt": f"Опиши проблему '{idea['problem']}' из области '{idea['group']}', не описывай решение, создай заголовок и выдели его одиночными обратными апострофами, сократи текст до тезизов 'Как было', 'Причины', 'Что сделано', 'Результат', используй не более {self.topic_word_limit} слов",
+                    "description_image": f"Нарисуй рисунок, вдохновлённый проблемой '{idea['problem']}' из области '{idea['group']}'",
+                    "solution_prompt": f"Опиши решение проблемы '{idea['problem']}' с помощью техники '{idea['technic']}' из области '{idea['group']}', создай заголовок и выдели его одиночными обратными апострофами, используй не более {self.topic_word_limit} слов",
+                    "solution_image": f"Нарисуй рисунок, вдохновлённый темой '{idea['technic']}' из области '{idea['group']}'",
+                }
+
+    def __init__(self, **kwargs):
+        super().__init__(
+            first_post_date=datetime(2025, 2, 24),
+            text_generator=OpenAITextGenerator(),
+            topic_word_limit=300,
+            days_between_posts=timedelta(days=3),
+            reviewer=TelegramSender(channel_id=-1002312034777),
+            senders=[TelegramSender(channel_id=f"@NextToTheFuture"), InstagramSender(channel_token_name='FUTUREUNLOCKED_OFFICIAL_TOKEN')],
+            **kwargs
+        )
+
+    
--- send.py
+++ send.py
@@ -1,41 +1,12 @@
-import os
-import telebot
-import json
-import glob
+from project import Project
 import schedule
 import time
 
-from datetime import date
-from datetime import timedelta
-from helpers.textes import gen_text
-from helpers.images import gen_image
-
-BOT_TOKEN_NAME = "ATHE_BOT_TOKEN"
-BOT_TOKEN = os.environ.get(BOT_TOKEN_NAME)
-CHAT_ID = '@NextToTheFuture'
-
-def job(CHAT_ID=CHAT_ID, text_gen=False, image_gen=False, problem=True, offset = 0):
-    check_date = date.today() + timedelta(days=offset)
-    tasks = json.load(open('files/in_progress.json', 'rt', encoding='UTF-8'))
-    for task in tasks:
-        if task["date"] == check_date.strftime('%Y-%m-%d'):
-            folder_name = glob.escape(f"files/data/{task['group'].replace('/', ',')}/{task['name'].replace('/', ',')}")
-            text_file_name = f"{folder_name}/{'problem' if problem else 'solution'}.txt"
-            image_file_name = f"{folder_name}/{'problem' if problem else 'solution'}.png"
-
-            if text_gen: gen_image(task)
-            if image_gen: gen_text(task)            
-
-            bot = telebot.TeleBot(BOT_TOKEN)
-            if os.path.exists(image_file_name):
-                bot.send_photo(chat_id=CHAT_ID, photo=open(image_file_name, 'rb'), parse_mode="Markdown", disable_notification=True)
-
-            if os.path.exists(text_file_name):
-                bot.send_message(chat_id=CHAT_ID, text=open(text_file_name, 'rt', encoding='UTF-8').read(), parse_mode="Markdown")
 
 if __name__ == '__main__':
-    schedule.every().day.at("09:00",'Europe/Moscow').do(job, problem=True)
-    schedule.every().day.at("12:00",'Europe/Moscow').do(job, problem=False)
+    p = Project()
+    schedule.every().day.at("09:00",'Europe/Moscow').do(p, type='problem')
+    schedule.every().day.at("12:00",'Europe/Moscow').do(p, type='solution')
 
     half_day = 12 * 60 * 60
 
